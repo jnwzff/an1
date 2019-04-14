83c7cfc2407e49a8edbc50ac3af17391ef0f1098
jnwzff <1984503137@qq.com>
2019-04-14 16:22:34 +0800

    all

--- webview/src/main/java/com/example/webview/MainActivity.java
+++ webview/src/main/java/com/example/webview/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.webview;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webView);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.loadUrl("file://");
+
     }
 }
